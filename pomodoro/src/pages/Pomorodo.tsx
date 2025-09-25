68f73d5eff7636b98b203f574e506b9c05dfb56d
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-25 11:52:07 +0200

    setting roots
    index css update

--- pomodoro/src/pages/Pomorodo.tsx
+++ pomodoro/src/pages/Pomorodo.tsx
@@ -1,7 +1,9 @@
 export function Pomodoro() {
   return (
     <div className="w-screen h-screen bg-gray flex flex-col justify-center items-center">
-      <h1 className="text-pink-500 bg-gray">Pomodoro Timer testing tailwind</h1>
+      <h1 className="text-[var(--text-hint-inverse)] font-bold text-2xl mb-5">
+        Pomodoro Timer testing tailwind
+      </h1>
       <p>Welcome to pomodoro page</p>
     </div>
   );
