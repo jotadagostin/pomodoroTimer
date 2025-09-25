a75c48fe4c2fc9600ccfc17a5ced910a219c185c
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-25 17:20:39 +0200

    setting pomodoro css

--- pomodoro/src/pages/Pomorodo.tsx
+++ pomodoro/src/pages/Pomorodo.tsx
@@ -1,10 +1,38 @@
+import restartSvg from "../assets/restart.svg";
+import configSvg from "../assets/configs.svg";
+
 export function Pomodoro() {
   return (
-    <div className="w-screen h-screen bg-gray flex flex-col justify-center items-center">
-      <h1 className="text-[var(--text-hint-inverse)] font-bold text-2xl mb-5">
-        Pomodoro Timer testing tailwind
-      </h1>
-      <p>Welcome to pomodoro page</p>
+    <div className=" bg-[var(--bg-paper)] flex flex-col justify-center items-center w-[412px] h-[256px] rounded-xl">
+      <header className="w-full h-full flex flex-col items-center">
+        <nav className="flex gap-20">
+          <button className=" text-[var(--text-hint)] hover:border-b-2 hover:border-[var(--text-default)]">
+            Focus
+          </button>
+          <button className=" text-[var(--text-hint)] hover: hover:border-b-2 hover:border-[var(--text-default)] ">
+            Break
+          </button>
+          <button className=" text-[var(--text-hint)] hover: hover:border-b-2 hover:border-[var(--text-default)]">
+            Rest
+          </button>
+        </nav>
+      </header>
+      <main className="p-6">
+        <div>
+          <span>30</span>
+          <span>:</span>
+          <span>00</span>
+        </div>
+        <div>
+          <button>
+            <img src={restartSvg} alt="" />
+          </button>
+          <button>Start</button>
+          <button>
+            <img src={configSvg} alt="" />
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
