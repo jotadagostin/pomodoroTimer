6a1317fa10d996d80895f6d0e8e7eff4e0ae5d4e
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-25 11:13:07 +0200

    setting react and routes

---
+++ pomodoro/src/pages/Pomorodo.tsx
@@ -0,0 +1,8 @@
+export function Pomodoro() {
+  return (
+    <div className="w-screen h-screen bg-gray flex flex-col justify-center items-center">
+      <h1 className="text-pink-500 bg-gray">Pomodoro Timer testing tailwind</h1>
+      <p>Welcome to pomodoro page</p>
+    </div>
+  );
+}
---
+++ pomodoro/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router";
+import { Pomodoro } from "../pages/Pomorodo";
+import Configs from "../pages/Setting";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Pomodoro />} />
+      <Route path="/configs" element={<Configs />} />
+      <Route />
+    </Routes>
+  );
+}
