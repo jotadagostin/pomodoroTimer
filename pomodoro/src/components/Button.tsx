7944f5927b5256cc9ea7e1208cf93737ecfa6f18
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-26 12:47:21 +0200

    pomorodo.tsx update
    css ok

---
+++ pomodoro/src/components/Button.tsx
@@ -0,0 +1,9 @@
+export function Button() {
+  return (
+    <div>
+      <button className="text-[var(--text-default-inverse)] bg-[var(--text-primary)] px-6 py-2 rounded-md cursor-pointer hover:bg-[var(--bg-primary)]">
+        Start
+      </button>
+    </div>
+  );
+}
--- pomodoro/src/pages/Pomorodo.tsx
+++ pomodoro/src/pages/Pomorodo.tsx
@@ -1,35 +1,49 @@
 import restartSvg from "../assets/restart.svg";
 import configSvg from "../assets/configs.svg";
+import { Button } from "../components/Button";
 
 export function Pomodoro() {
   return (
     <div className=" bg-[var(--text-default-inverse)] flex flex-col justify-between items-center w-[412px] h-[256px] rounded-xl">
-      <header className="w-[412px] h-[45px]   flex flex-col items-center justify-center">
-        <nav className="flex gap-20">
-          <button className=" text-[var(--text-hint)] hover:border-b-2 hover:border-[var(--text-default)]">
+      <header className="w-[412px] h-[45px]  flex flex-col items-center justify-center border-b-1 border-[var(--bg-paper)] ">
+        <nav className="w-[412px] flex  justify-around">
+          <button
+            className="relative text-[var(--text-hint)] pb-3 pt-3
+  after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-[var(--text-default)] after:transition-all after:duration-200
+  hover:after:w-[250%] hover:after:-translate-x-3/10 hover:text-[var(--text-default)] "
+          >
             Focus
           </button>
-          <button className=" text-[var(--text-hint)] hover: hover:border-b-2  hover:border-[var(--text-default)] ">
+          <button
+            className=" relative text-[var(--text-hint)] pb-3 pt-3
+  after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-[var(--text-default)] after:transition-all after:duration-200
+  hover:after:w-[250%] hover:after:-translate-x-3/10 hover:text-[var(--text-default)] "
+          >
             Break
           </button>
-          <button className=" text-[var(--text-hint)] hover: hover:border-b-2 hover:border-[var(--text-default)]">
+          <button
+            className=" relative text-[var(--text-hint)] pb-3 pt-3
+  after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-[var(--text-default)] after:transition-all after:duration-200
+  hover:after:w-[250%] hover:after:-translate-x-3/10 hover:text-[var(--text-default)] "
+          >
             Rest
           </button>
         </nav>
       </header>
-      <main className="w-[412px] h-[205px] flex flex-col justify-center items-center">
-        <div className="w-[186px] h-[77px] flex  justify-center items-center">
+      <main className="w-[412px] flex-1 flex flex-col justify-center items-center gap-5">
+        <div className="w-[186px]  flex  justify-center items-center">
           <span>30</span>
           <span>:</span>
           <span>00</span>
         </div>
-        <div className="w-[192px] h-[40px] flex justify-center items-center gap-2.5">
-          <button>
-            <img src={restartSvg} alt="" />
+        <div className=" w-[412px] h-[45px]  flex  justify-center items-center gap-5.5">
+          <button className="cursor-pointer hover:brightness-0 hover:invert">
+            <img src={restartSvg} alt="restart icon" />
           </button>
-          <button className="bg-[var(--text-primary)]">Start</button>
-          <button>
-            <img src={configSvg} alt="" />
+
+          <Button />
+          <button className="cursor-pointer hover:brightness-0 hover:invert duration-200">
+            <img src={configSvg} alt="config icon" />
           </button>
         </div>
       </main>
