59a664e28da17048a0c9012dbbf818402a02f4a8
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-26 10:31:15 +0200

    setting button

--- pomodoro/src/pages/Pomorodo.tsx
+++ pomodoro/src/pages/Pomorodo.tsx
@@ -3,13 +3,13 @@ import configSvg from "../assets/configs.svg";
 
 export function Pomodoro() {
   return (
-    <div className=" bg-[var(--bg-paper)] flex flex-col justify-center items-center w-[412px] h-[256px] rounded-xl">
-      <header className="w-full h-full flex flex-col items-center">
+    <div className=" bg-[var(--text-default-inverse)] flex flex-col justify-between items-center w-[412px] h-[256px] rounded-xl">
+      <header className="w-[412px] h-[45px]   flex flex-col items-center justify-center">
         <nav className="flex gap-20">
           <button className=" text-[var(--text-hint)] hover:border-b-2 hover:border-[var(--text-default)]">
             Focus
           </button>
-          <button className=" text-[var(--text-hint)] hover: hover:border-b-2 hover:border-[var(--text-default)] ">
+          <button className=" text-[var(--text-hint)] hover: hover:border-b-2  hover:border-[var(--text-default)] ">
             Break
           </button>
           <button className=" text-[var(--text-hint)] hover: hover:border-b-2 hover:border-[var(--text-default)]">
@@ -17,17 +17,17 @@ export function Pomodoro() {
           </button>
         </nav>
       </header>
-      <main className="p-6">
-        <div>
+      <main className="w-[412px] h-[205px] flex flex-col justify-center items-center">
+        <div className="w-[186px] h-[77px] flex  justify-center items-center">
           <span>30</span>
           <span>:</span>
           <span>00</span>
         </div>
-        <div>
+        <div className="w-[192px] h-[40px] flex justify-center items-center gap-2.5">
           <button>
             <img src={restartSvg} alt="" />
           </button>
-          <button>Start</button>
+          <button className="bg-[var(--text-primary)]">Start</button>
           <button>
             <img src={configSvg} alt="" />
           </button>
